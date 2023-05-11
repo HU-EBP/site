dea42ceb90d0140f6c499ad023663e96f88f8b80
Tobias <113168391+tobiasvdorp@users.noreply.github.com>
2023-05-11 16:26:48 +0200

    Game toegevoegd & werkend
    Werkt ook op safari

--- src/App.js
+++ src/App.js
@@ -3,8 +3,28 @@ import React from "react";
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <p>Dit is een mooie tekst</p>
+      <div id="game-container">
+        <h1>SPARK</h1>
+        <iframe
+          id="game"
+          title="Game"
+          mozallowfullscreen="true"
+          allow="autoplay; fullscreen"
+          src="https://hu-ebp.github.io/game/index.html"
+          style={{
+            marginWidth: "320px",
+            height: "650px",
+            width: "960px",
+          }}
+          name="SPARK"
+          msallowfullscreen="true"
+          allowFullScreen="true"
+          webkitallowfullscreen="true"
+          allowtransparency="true"
+          height="540px"
+          width="960px"
+        ></iframe>
+      </div>
     </div>
   );
 }
