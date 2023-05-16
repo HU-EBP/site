1b8e7ac6959fb769e52164456b2019016ce92193
Tobias <113168391+tobiasvdorp@users.noreply.github.com>
2023-05-16 13:05:02 +0200

    gitignore aangepast

---
+++ src/components/Navbar.js
@@ -0,0 +1,48 @@
+import React, { useRef } from "react";
+import {
+  FaBars,
+  FaGamepad,
+  FaHome,
+  FaQuestion,
+  FaSpeakap,
+  FaTimes,
+} from "react-icons/fa";
+import "../components/navbar.css";
+
+function Navbar() {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
+  return (
+    <header>
+      <h3>Logo</h3>
+      <nav ref={navRef}>
+        <a href="/#">
+          <FaHome /> Home
+        </a>
+        <a href="/#">
+          <FaGamepad /> Spark
+        </a>
+        <a href="/#">
+          <FaQuestion /> FAQ
+        </a>
+        <a href="/#">
+          <FaSpeakap /> Forum
+        </a>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button lassName="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
+  );
+}
+
+export default Navbar;
+
+// Test
