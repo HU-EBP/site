e0feedf297ae5d49e627155158dfdfade3aa0ee7
Tobias <113168391+tobiasvdorp@users.noreply.github.com>
2023-05-16 12:34:14 +0200

    test
    n

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
 import React from "react";
+import GamePlayer from "./components/GamePlayer";
 
 function App() {
   return (
     <div>
-      <div id="game-container">
-        <h1>SPARK</h1>
-        <iframe
-          id="game"
-          title="Game"
-          mozallowfullscreen="true"
-          allow="autoplay; fullscreen"
-          src="https://hu-ebp.github.io/game/index.html"
-          style={{
-            marginWidth: "320px",
-            height: "650px",
-            width: "960px",
-          }}
-          name="SPARK"
-          msallowfullscreen="true"
-          allowFullScreen="true"
-          webkitallowfullscreen="true"
-          allowtransparency="true"
-          height="540px"
-          width="960px"
-        ></iframe>
-      </div>
+      <GamePlayer />
     </div>
   );
 }
