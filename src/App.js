fae6e8e20a0be18775dba216cb8f2f2f20938718
Tobias <113168391+tobiasvdorp@users.noreply.github.com>
2023-05-17 10:14:49 +0200

    call to action verbeterd en iframe meer responsive gemaakt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
 import GamePlayer from "./components/GamePlayer";
 import Navbar from "./components/Navbar";
-import CallToAction from "./components/CallToAction";
+import "./components/CallToAction.css";
 
 function App() {
   return (
     <div>
+      {/* Navbar */}
       <Navbar />
-      <CallToAction />
+
+      {/* Call to action */}
+      <div id="cta-container">
+        <div className="cta-flex">
+          <h1>Spark</h1>
+          <button className="playbutton">Play</button>
+        </div>
+      </div>
+
+      {/* Gameplayer */}
       <GamePlayer />
+      <div></div>
     </div>
   );
 }
--- src/components/GamePlayer.js
+++ src/components/GamePlayer.js
@@ -9,18 +9,11 @@ function GamePlayer() {
         mozallowfullscreen="true"
         allow="autoplay; fullscreen"
         src="https://hu-ebp.github.io/game/index.html"
-        style={{
-          marginWidth: "320px",
-          height: "650px",
-          width: "960px",
-        }}
         name="SPARK"
         msallowfullscreen="true"
         allowFullScreen="true"
         webkitallowfullscreen="true"
         allowtransparency="true"
-        height="1080px"
-        width="1920px"
       ></iframe>
     </div>
   );
