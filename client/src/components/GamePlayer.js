28e7f960bdd02466ebbd80bb23a6429e5262ddfa
Tobias <113168391+tobiasvdorp@users.noreply.github.com>
2023-05-30 15:12:00 +0200

    kleine aanpassing in playbutton, gameplayer inactief gemaakt

--- client/src/components/GamePlayer.js
+++ client/src/components/GamePlayer.js
@@ -1,59 +1,50 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 
-function GamePlayer() {
-  const [isMobile, setIsMobile] = useState(null);
+// function GamePlayer() {
+//   const [isMobile, setIsMobile] = useState(null);
 
-  useEffect(() => {
-    setIsMobile(
-      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-        navigator.userAgent
-      )
-    );
-  }, []);
+//   useEffect(() => {
+//     setIsMobile(
+//       /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+//         navigator.userAgent
+//       )
+//     );
+//   }, []);
 
-  if (isMobile === null) {
-    return (
-      <>
-        <div className="loader flex-center">
-          <h2>Loading game frame...</h2>
-        </div>
-      </>
-    );
-  } else
-    return (
-      <>
-        <div id="game-container">
-          {isMobile ? (
-            <>
-              <h1 className="thegametitle">THE GAME</h1>
-              <div className="flex-center">
-                <div className="gameframe-mobile flex-center">
-                  <h2>Spark is currently not available on mobile devices.</h2>
-                </div>
-              </div>
-            </>
-          ) : (
-            <>
-              <h1 className="thegametitle">THE GAME</h1>
-              <div className="game-container">
-                <iframe
-                  id="game"
-                  title="Game"
-                  mozallowfullscreen="true"
-                  allow="autoplay; fullscreen"
-                  src="https://hu-ebp.github.io/game/index.html"
-                  name="SPARK"
-                  msallowfullscreen="true"
-                  allowFullScreen="true"
-                  webkitallowfullscreen="true"
-                  allowtransparency="true"
-                ></iframe>
-              </div>
-            </>
-          )}
-        </div>
-      </>
-    );
-}
+//   return (
+//     <>
+//       <div id="game-container">
+//         {isMobile ? (
+//           <>
+//             <h1 className="thegametitle">THE GAME</h1>
+//             <div className="flex-center">
+//               <div className="gameframe-mobile flex-center">
+//                 <h2>Spark is currently not available on mobile devices.</h2>
+//               </div>
+//             </div>
+//           </>
+//         ) : (
+//           <>
+//             <h1 className="thegametitle">THE GAME</h1>
+//             <div className="game-container">
+//               <iframe
+//                 id="game"
+//                 title="Game"
+//                 mozallowfullscreen="true"
+//                 allow="autoplay; fullscreen"
+//                 src="https://hu-ebp.github.io/game/index.html"
+//                 name="SPARK"
+//                 msallowfullscreen="true"
+//                 allowFullScreen="true"
+//                 webkitallowfullscreen="true"
+//                 allowtransparency="true"
+//               ></iframe>
+//             </div>
+//           </>
+//         )}
+//       </div>
+//     </>
+//   );
+// }
 
-export default GamePlayer;
+// export default GamePlayer;
