13f93322ce5d7372cb4aa420977e3a470a662dc2
Melvin0700 <113168782+Melvin0700@users.noreply.github.com>
2023-06-13 12:06:10 +0200

    aanpassingen

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,10 +1,10 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import "../components/contact.css";
-import React, { useState, useRef } from 'react';
-import emailjs from 'emailjs-com';
 
-export const Contact = () => {
+
+export const ContactUs = () => {
   const form = useRef();
-  const [showAlert, setShowAlert] = useState(false);
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -12,31 +12,24 @@ export const Contact = () => {
     emailjs.sendForm('gmail', 'template_ck3w84r', form.current, 'hKyvDQkLDIozlHXyo')
       .then((result) => {
           console.log(result.text);
-          setShowAlert(true); // Stel showAlert in op true na succesvol verzenden van de e-mail
       }, (error) => {
           console.log(error.text);
       });
   };
 
   return (
-    <div>
-      {showAlert && (
-        <div className="alert alert-success">
-          Mail verzonden!
-        </div>
-      )}
-      <form ref={form} onSubmit={sendEmail} className="contact-page">
-        <h1>
+    <form ref={form} onSubmit={sendEmail} className="contact-page">
+    <h1>
           Contact <span>Here</span>
         </h1>
-        <input type="email" name="user_email" placeholder="Example@gmail.com" />
-        <input type="text" name="user_name" placeholder="Enter Name" />
-        <input type="text" name="subject" placeholder="Enter Subject" />
-        <textarea className="message" id="30" rows="10" placeholder="type here..." />
-        <button value="send" type="submit">send</button>
-      </form>
-    </div>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea className="message"  name="message" />
+      <button value="send" type="submit">send</button>
+    </form>
   );
 };
 
-export default Contact;
+ export default ContactUs;
