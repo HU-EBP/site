43d20f8b17447ec0e5cbc73daf268e8c461ed614
Melvin0700 <113168782+Melvin0700@users.noreply.github.com>
2023-06-13 16:16:40 +0200

    Email verzonden via emailjs

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,7 +1,6 @@
 import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import emailjs from 'emailjs-com';
 import "../components/contact.css";
-
 
 export const ContactUs = () => {
   const form = useRef();
@@ -9,27 +8,31 @@ export const ContactUs = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('gmail', 'template_ck3w84r', form.current, 'hKyvDQkLDIozlHXyo')
+    emailjs.sendForm('service_34ej9ku', 'template_ck3w84r', form.current, 'i2njx-0K_ARFwMto2')
       .then((result) => {
-          console.log(result.text);
+        console.log(result.text);
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
+
+    // Reset het formulier na verzenden
+    form.current.reset();
   };
 
   return (
     <form ref={form} onSubmit={sendEmail} className="contact-page">
-    <h1>
-          Contact <span>Here</span>
-        </h1>
+      <h1>
+        Contact <span>Here</span>
+      </h1>
+      <label>Naam</label>
       <input type="text" name="user_name" />
       <label>Email</label>
       <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea className="message"  name="message" />
-      <button value="send" type="submit">send</button>
+      <label>Bericht</label>
+      <textarea className="message" name="message" />
+      <button type="submit">Verzenden</button>
     </form>
   );
 };
 
- export default ContactUs;
+export default ContactUs;
